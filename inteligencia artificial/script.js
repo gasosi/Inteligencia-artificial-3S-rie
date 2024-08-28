5ed0c4c534dab6765e2e7ac3e5f3f7137486e547
Gabriel Soares <115032490+gasosi@users.noreply.github.com>
2024-08-28 16:36:03 -0300

    sdfsd

--- inteligencia artificial/script.js
+++ inteligencia artificial/script.js
@@ -7,36 +7,39 @@ const textoResultado = document.querySelector(".texto-resultado");
 const perguntas = [
     {
         enunciado: "Como um torcedor pode afetar uma estação de trem inteira por causa de uma aposta errada?",
-
-        alternativas: ["Alternativa 1",
-            "Alternativa 2"
+        alternativas: [
+            "Fazer com que o ambiente seja mais perigoso e violento, podendo resultar em feridos e até óbitos.",
+            "Pode quebrar uma coisa ou outra da estação, mas logo pode ser contido e preso pela polícia."
         ]
     },
-
     {
-        enunciado: "Pergunta2",
-        alternativas: ["Alternativa 1",
-            "Alternativa 2"
+        enunciado: "Como as apostas esportivas podem influenciar na taxa de violência urbana em transportes públicos?",
+        alternativas: [
+            "Algumas apostas esportivas são associadas a organizações ou grupos que podem promover ou tolerar comportamentos violentos.  ",
+            "Áreas onde há muitos locais para apostas esportivas podem ter uma concentração maior de pessoas envolvidas em apostas, o que pode aumentar a tensão e, potencialmente, a violência, incluindo transportes públicos.",
         ]
-    }, 
+    },
     {
-        enunciado: "Pergunta2",
-        alternativas: ["Alternativa 1",
-            "Alternativa 2"
+        enunciado: "Após a elaboração do trabalho, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        alternativas: [
+            "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+            "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores."
         ]
-    }, 
+    },
     {
-        enunciado: "Pergunta2",
-        alternativas: ["Alternativa 1",
-            "Alternativa 2"
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        alternativas: [
+            "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+            "Criar uma imagem utilizando um gerador de imagem de IA."
         ]
-    }, 
+    },
     {
-        enunciado: "Pergunta2",
-        alternativas: ["Alternativa 1",
-            "Alternativa 2"
+        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda de uma IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz?",
+        alternativas: [
+           "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
+            "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial."
         ]
-    }, 
+    },
 ];
 
 let atual = 0;
@@ -46,15 +49,13 @@ function mostraPergunta() {
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
     mostraAlternativas();
-    
 }
- 
-function mostraAlternativas(){
-    for(const alternativas of perguntaAtual.alternativas){
+
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa;
         caixaAlternativas.appendChild(botaoAlternativas);
-
     }
 }
 
