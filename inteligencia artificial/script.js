0f2479b6a926c7a6b11f976e60d869ae13a03fa1
Gabriel Soares <115032490+gasosi@users.noreply.github.com>
2024-09-19 10:42:59 -0300

    ertgwe

--- inteligencia artificial/script.js
+++ inteligencia artificial/script.js
@@ -59,9 +59,13 @@ function mostraPergunta() {
 }
 
 function mostraAlternativas() {
-    for (const alternativa of perguntaAtual.alternativas) {
+    for (const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa;
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", function(){
+            atual++;
+            mostraPergunta();   
+        })
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
